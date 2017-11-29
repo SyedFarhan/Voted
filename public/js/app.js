de0d82bd64751a7ce5131949de12439919b754ad
Syed Farhan <syed.farhan@ge.com>
2017-11-29 16:42:29 -0500

    Created a function to handle upvotes

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,13 @@
 class Product extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleUpVote = this.handleUpVote.bind(this);
+  }
+  handleUpVote() {
+    this.props.onVote(this.props.id);
+  }
+
   render() {
     return (
       <div className='item'>
@@ -7,7 +16,7 @@ class Product extends React.Component {
         </div>
         <div className='middle aligned content'>
           <div className='header'>
-            <a>
+            <a onClick={this.handleUpVote}>
               <i className='large caret up icon' />
             </a>
             {this.props.votes}
@@ -33,7 +42,16 @@ class Product extends React.Component {
 }
 
 class ProductList extends React.Component {
+  handleProductUpVote(productId) {
+    console.log(productId + ' was upvoted.');
+  }
+
   render() {
+
+    const products = Seed.products.sort((a, b) => (
+      b.votes - a.votes
+    ));
+
     const productComponents = Seed.products.map((product) => (
       <Product
         key={'product-' + product.id}
@@ -44,8 +62,10 @@ class ProductList extends React.Component {
         votes={product.votes}
         submitterAvatarUrl={product.submitterAvatarUrl}
         productImageUrl={product.productImageUrl}
+        onVote={this.handleProductUpVote}
       />
     ));
+
     return (
       <div className='ui unstackable items'>
         {productComponents}
