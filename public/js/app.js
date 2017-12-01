6da3a9b11a370ea994f1882cc7043a4bf10c2695
Syed Farhan <syed.farhan@ge.com>
2017-12-01 15:41:19 -0500

    Refactored Product component to use property intializers

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,8 @@
 class Product extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.handleUpVote = this.handleUpVote.bind(this);
-  }
-
-  handleUpVote() {
-    this.props.onVote(this.props.id);
-  }
+  handleUpVote = () => (
+    this.props.onVote(this.props.id)
+  );
 
   render() {
     return (
